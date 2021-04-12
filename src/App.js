aa35b06a34ab1a1602c823bfbd2c99ecad349231
Mark Harrop <harropm@rogers.com>
2021-04-12 17:04:03 -0400

    updated population of post comments and emotions

--- src/App.js
+++ src/App.js
@@ -42,13 +42,13 @@ function App() {
      
         <ul>
           {
-            stories.map((story, index) => {
-              console.log(story, index);
+            stories.map((story) => {
+              console.log(story);
               return(
 
-                <li index={index}>
-                  <h3>I am posting this story because I am feeling ____</h3>
-                  <p>Paragraph from the post goes here.  I am trying to figure out how to grab the text from Firebase to populate it!</p>
+                <li tabIndex="0">
+                  <h3>I am posting this story because I am feeling {story.emotion}</h3>
+                  <p>{story.post}</p>
                 </li>
               )
             })
